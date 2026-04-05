13332a1c4c4804dd59fb9618e875e6aacfb6aaba
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-04 22:00:41 -0500

    feat: agrega clase ItemCarrito con atributos y metodos base

--- src/mundo/ItemCarrito.java
+++ src/mundo/ItemCarrito.java
@@ -2,5 +2,55 @@ package mundo;
 
 public class ItemCarrito 
 {
+	private Producto producto;
+	private int cantidad;
+	
+	public ItemCarrito(Producto producto) 
+	{
+		this.producto = producto;
+		this.cantidad = 1;
+	}
 
+	public Producto getProducto() 
+	{
+		return producto;
+	}
+
+
+	public int getCantidad() 
+	{
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) 
+	{
+		this.cantidad = cantidad;
+	}
+	
+	public void setProducto(Producto producto) 
+	{
+		this.producto = producto;
+	}
+	
+	public void incrementarCantidad()
+	{
+		this.cantidad++;
+	}
+	
+	public void decrementarCantidad()
+	{
+		if(cantidad > 1)
+		{
+			this.cantidad--;
+		}
+	}
+	
+	public double getSubtotal()
+	{
+		return producto.getPrecioFinal() * cantidad;
+	}
+	
+	
+	
+	
 }
