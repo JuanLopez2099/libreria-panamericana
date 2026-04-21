421be793a0b0c397c4ec5631c77b0cbd397f4e9b
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-21 17:57:07 -0500

    feat: PanelLibroActual agregado

--- src/interfaz/PanelListaLibros.java
+++ src/interfaz/PanelListaLibros.java
@@ -1,5 +1,57 @@
 package interfaz;
 
-public class PanelListaLibros {
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+
+public class PanelListaLibros extends JPanel
+{	
+	private JTextArea listaLibros;
+	private PanelLibroActual libroActual;
+	
+	
+	public PanelListaLibros()
+	{
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		listaLibros = new JTextArea();
+		JScrollPane scroll = new JScrollPane(listaLibros);
+		
+		TitledBorder borde = BorderFactory.createTitledBorder("Lista de Libros");
+		borde.setTitleColor(Color.BLACK);
+		scroll.setBorder(borde);
+		
+		libroActual = new PanelLibroActual();
+		
+		
+        gbc.gridy = 0;
+        gbc.gridheight = 6;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        
+        gbc.gridx = 0;
+        gbc.weightx = 0.4;
+        add(scroll, gbc);
+        
+        gbc.gridx = 1;
+        gbc.weightx = 0.6;
+        add(libroActual, gbc);
+       
+		
+	}
+	
+	
+	
+	
 
 }
