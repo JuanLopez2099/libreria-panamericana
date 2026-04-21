6b01082c3792201bfeb99268ceb44b206bba0a43
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-21 16:12:53 -0500

    feat: Config de InterfazBiblioteca añadida

--- src/interfaz/InterfazBiblioteca.java
+++ src/interfaz/InterfazBiblioteca.java
@@ -1,5 +1,32 @@
 package interfaz;
 
-public class InterfazBiblioteca {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class InterfazBiblioteca extends JFrame
+{
+	private PanelListaLibros panelListaLibros;
+	private PanelConsultas panelConsultas;
+	
+	public InterfazBiblioteca()
+	{
+		setTitle("Bibliteca Panamericana");
+		setSize(950, 600);
+		setLayout(new BorderLayout());
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		InterfazBiblioteca ventana = new InterfazBiblioteca();
+		
+		ventana.setVisible(true);
+		
+	}
 
 }
