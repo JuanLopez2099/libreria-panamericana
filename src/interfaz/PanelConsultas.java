bfef22d6df813b806ffe042be4fa50b6d1b0aa45
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-21 19:17:51 -0500

    feat: PanelConsultas con codigo implementado

--- src/interfaz/PanelConsultas.java
+++ src/interfaz/PanelConsultas.java
@@ -1,5 +1,89 @@
 package interfaz;
 
-public class PanelConsultas {
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class PanelConsultas extends JPanel
+{
+	private JButton buscarCodigo;
+	private JButton buscarTitulo;
+	private JButton filtrarCategoria;
+	private JButton filtrarAutor;
+	private JButton filtrarDisponibles;
+	private JButton filtrarDescuento;
+	private JButton ordenarPrecio;
+	private JButton ordenarTitulo;
+	private JLabel lblcodigo;
+	private JTextField txtcodigo;
+	private JLabel lbltitulo;
+	private JTextField txttitulo;
+	
+	
+	public PanelConsultas()
+	{
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		TitledBorder borde = BorderFactory.createTitledBorder("Consultas");
+		borde.setTitleColor(Color.BLACK);
+		setBorder(borde);
+		
+		buscarCodigo = new JButton("Buscar Por Codigo");
+		buscarCodigo = new JButton("Buscar Por Código");
+		buscarTitulo = new JButton("Buscar Por Título");
+		filtrarCategoria = new JButton("Filtrar por Categoría");
+		filtrarAutor = new JButton("Filtrar por Autor");
+		filtrarDisponibles = new JButton("Mostrar Disponibles");
+		filtrarDescuento = new JButton("Mostrar con Descuento");
+		ordenarPrecio = new JButton("Ordenar por Precio");
+		ordenarTitulo = new JButton("Ordenar por Título");
+		
+		lblcodigo = new JLabel("Codigo:");
+		txtcodigo = new JTextField("");
+		lbltitulo = new JLabel("Titulo:");
+		txttitulo = new JTextField("");
+		
+		 gbc.gridx = 0;
+	     gbc.weightx = 1.0;
+	     gbc.fill = GridBagConstraints.HORIZONTAL;
+	     
+	     gbc.gridy = 0;
+	     add(buscarCodigo, gbc);
+
+	     gbc.gridy = 1;
+	     add(buscarTitulo, gbc);
+
+	     gbc.gridy = 2;
+	     add(filtrarCategoria, gbc);
+
+	     gbc.gridy = 3;
+	     add(filtrarAutor, gbc);
+
+	     gbc.gridy = 4;
+	     add(filtrarDisponibles, gbc);
+
+	     gbc.gridy = 5;
+	     add(filtrarDescuento, gbc);
+
+	     gbc.gridy = 6;
+	     add(ordenarPrecio, gbc);
+
+	     gbc.gridy = 7;
+	     add(ordenarTitulo, gbc);
+	     
+	     
+		
+		
+		
+		
+	}
+	
 }
