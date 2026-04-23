b3203f558d59f75f1576db4403914c0c4811a8f6
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-23 13:42:20 -0500

    feat: interfaz del JDialog para el carrito de compra implementada

--- src/interfaz/InterfazCarrito.java
+++ src/interfaz/InterfazCarrito.java
@@ -1,5 +1,36 @@
 package interfaz;
 
-public class InterfazCarrito {
+import java.awt.BorderLayout;
+
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+
+public class InterfazCarrito extends JDialog
+{
+	private PanelListaCarrito panelListaCarrito;
+	private JButton btncomprar;
+	
+	
+	public InterfazCarrito(JFrame padre)
+	{
+		super(padre, "Carrito de Compra", true);
+		setSize(700, 450);
+		setLayout(new BorderLayout());
+		setResizable(false);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(padre);
+		
+		panelListaCarrito = new PanelListaCarrito();
+		add(panelListaCarrito, BorderLayout.CENTER);
+		
+		btncomprar = new JButton("Comprar 🛍️");
+		
+		add(btncomprar, BorderLayout.SOUTH);
+	}
+
+
 
 }
