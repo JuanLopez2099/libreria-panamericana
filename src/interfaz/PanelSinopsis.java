17d37352ffdcfacb4bbd2613988abdb60605bdba
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-22 22:26:51 -0500

    feat: clase PanelSinopsis agregada con codigo implementado

---
+++ src/interfaz/PanelSinopsis.java
@@ -0,0 +1,44 @@
+package interfaz;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+public class PanelSinopsis extends JPanel
+{
+	private JTextArea txtsinopsis;
+	
+	
+	public PanelSinopsis()
+	{
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		TitledBorder borde = BorderFactory.createTitledBorder("Sinopsis");
+		borde.setTitleColor(Color.BLACK);
+		setBorder(borde);
+		
+		txtsinopsis = new JTextArea();
+		txtsinopsis.setEditable(false);
+		txtsinopsis.setLineWrap(true);        
+		txtsinopsis.setWrapStyleWord(true);
+		
+		JScrollPane scroll = new JScrollPane(txtsinopsis);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.fill = GridBagConstraints.BOTH;
+		add(scroll, gbc);
+		
+		
+	}
+
+}
