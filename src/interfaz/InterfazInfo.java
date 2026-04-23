521e692dfae16bd1d8754630c60628e63474e7e4
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-22 22:27:15 -0500

    feat: PanelSinopsis implementado a la interfaz JDialog

--- src/interfaz/InterfazInfo.java
+++ src/interfaz/InterfazInfo.java
@@ -3,6 +3,7 @@ package interfaz;
 import java.awt.BorderLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 
 
 
@@ -11,17 +12,21 @@ public class InterfazInfo extends JDialog
 	private PanelInfoLibro panelInfoLibro;
 	private PanelSinopsis panelSinopsis;
 	
-	public InterfazInfo()
+	public InterfazInfo(JFrame padre)
 	{
-		setTitle("Informacion del Libro");
+		super(padre, "Informacion del Libro", true);
 		setSize(700, 500);
 		setLayout(new BorderLayout());
 		setResizable(false);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setLocationRelativeTo(null);
+		setLocationRelativeTo(padre);
 		
 		panelInfoLibro = new PanelInfoLibro();
 		add(panelInfoLibro, BorderLayout.NORTH);
+		
+		panelSinopsis = new PanelSinopsis();
+		add(panelSinopsis, BorderLayout.CENTER);
+		
 		
 		
 		
