dc28de86c272bf841acc89eb5436f79f7e3827ed
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-22 22:18:29 -0500

    feat: panel de informacion del libro implementado a la interfaz

--- src/interfaz/InterfazInfo.java
+++ src/interfaz/InterfazInfo.java
@@ -1,5 +1,30 @@
 package interfaz;
 
-public class InterfazInfo {
+import java.awt.BorderLayout;
+
+import javax.swing.JDialog;
+
+
+
+public class InterfazInfo extends JDialog
+{
+	private PanelInfoLibro panelInfoLibro;
+	private PanelSinopsis panelSinopsis;
+	
+	public InterfazInfo()
+	{
+		setTitle("Informacion del Libro");
+		setSize(700, 500);
+		setLayout(new BorderLayout());
+		setResizable(false);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		panelInfoLibro = new PanelInfoLibro();
+		add(panelInfoLibro, BorderLayout.NORTH);
+		
+		
+		
+	}
 
 }
