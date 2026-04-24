fe4057b7a31646c6648612a311714d5dfa723abe
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-04-23 21:57:33 -0500

    feat: se llaman los metodos para añadir la info a cada panel

--- src/interfaz/InterfazInfo.java
+++ src/interfaz/InterfazInfo.java
@@ -5,6 +5,8 @@ import java.awt.BorderLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 
+import mundo.Producto;
+
 
 
 public class InterfazInfo extends JDialog
@@ -12,7 +14,7 @@ public class InterfazInfo extends JDialog
 	private PanelInfoLibro panelInfoLibro;
 	private PanelSinopsis panelSinopsis;
 	
-	public InterfazInfo(JFrame padre)
+	public InterfazInfo(JFrame padre, Producto p)
 	{
 		super(padre, "Informacion del Libro", true);
 		setSize(1050, 600);
@@ -22,9 +24,11 @@ public class InterfazInfo extends JDialog
 		setLocationRelativeTo(padre);
 		
 		panelInfoLibro = new PanelInfoLibro();
+		panelInfoLibro.mostrarInformacio(p);
 		add(panelInfoLibro, BorderLayout.NORTH);
 		
 		panelSinopsis = new PanelSinopsis();
+		panelSinopsis.mostrarSinopsis(p);
 		add(panelSinopsis, BorderLayout.CENTER);
 		
 		
